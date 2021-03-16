6383b04186145cc579a589a66f0e047cbcc0a6d4
Alpenbaer <77889741+Alpenbaer@users.noreply.github.com>
2021-03-16 20:07:55 +0100

    Rechner(geht doch)

--- rechner.js
+++ rechner.js
@@ -1,9 +1,9 @@
 function zwischentotal() {
-   var einer = document.getElementsByName('einer_1')[0];
-   var zweier = document.getElementsByName('zweier_2')[0];
-   var dreier = document.getElementsByName('dreier_3')[0];
-   var vierer = document.getElementsByName('vierer_4')[0];
-   var fünfer = document.getElementsByName('fünfer_5')[0];
-   var sechser = document.getElementsByName('sechser_6')[0];
+   var einer = document.getElementsByName('einer_1')[0].value;
+   var zweier = document.getElementsByName('zweier_2')[0].value;
+   var dreier = document.getElementsByName('dreier_3')[0].value;
+   var vierer = document.getElementsByName('vierer_4')[0].value;
+   var fünfer = document.getElementsByName('fünfer_5')[0].value;
+   var sechser = document.getElementsByName('sechser_6')[0].value;
    document.getElementsByName('zwischentotal')[0].value=einer*1 + zweier*1 + dreier*1 + vierer*1 + fünfer*1 + sechser*1;
  }
